62662fbab69fcf9c059a06009f920eb989f1fcb8
RFFZ <1748141337@qq.com>
2022-09-24 22:14:16 +0800

    timeWheel can run

--- timeWheel.cpp
+++ timeWheel.cpp
@@ -4,8 +4,27 @@
 #include <time.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <libgen.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <time.h>
+#include <unistd.h>
+
 
 #define BUFFER_SIZE 64
+#define MAX_EVENT_NUMBER 1024
+#define TIMESLOT 5
+#define FD_LIMIT 65535
+
+
+
 
 class tw_timer;
 
@@ -57,31 +76,30 @@ public:
         }
     }
 
-
-    tw_timer* add_timer(int timeout)
+    tw_timer *add_timer(int timeout)
     {
-        if(timeout < 0)
+        if (timeout < 0)
         {
             return NULL;
         }
 
         int ticks = 0;
-        if(timeout < SI)
+        if (timeout < SI)
         {
             ticks = 1;
         }
         else
         {
-            ticks = timeout/SI;
+            ticks = timeout / SI;
         }
 
         int rotation = timeout / N;
-        int ts = (cur_slot + (ticks%N))%N;
-        tw_timer* timer = new tw_timer(rotation,ts);
-        if(!slots[ts])
+        int ts = (cur_slot + (ticks % N)) % N;
+        tw_timer *timer = new tw_timer(rotation, ts);
+        if (!slots[ts])
         {
             printf("add timer,rotation is %d,ts is %d,cur_slot is %d\n",
-            rotation,ts,cur_slot);
+                   rotation, ts, cur_slot);
             slots[ts] = timer;
         }
         else
@@ -93,19 +111,18 @@ public:
         return timer;
     }
 
-
-    void del_timer(tw_timer* timer)
+    void del_timer(tw_timer *timer)
     {
-        if(!timer)
+        if (!timer)
         {
             return;
         }
 
         int ts = timer->time_slot;
-        if(timer == slots[ts])
+        if (timer == slots[ts])
         {
             slots[ts] = slots[ts]->next;
-            if(slots[ts])
+            if (slots[ts])
             {
                 slots[ts]->prev = NULL;
             }
@@ -114,7 +131,7 @@ public:
         else
         {
             timer->prev->next = timer->next;
-            if(timer->next)
+            if (timer->next)
             {
                 timer->next->prev = timer->prev;
             }
@@ -122,15 +139,14 @@ public:
         }
     }
 
-
     void tick()
     {
-        tw_timer* tmp = slots[cur_slot];
-        printf("current slot is %d\n",cur_slot);
-        while(tmp)
+        tw_timer *tmp = slots[cur_slot];
+        printf("current slot is %d\n", cur_slot);
+        while (tmp)
         {
             printf("tick the timer once\n");
-            if(tmp->rotation)
+            if (tmp->rotation)
             {
                 tmp->rotation--;
                 tmp = tmp->next;
@@ -138,12 +154,12 @@ public:
             else
             {
                 tmp->cb_func(tmp->user_data);
-                if(tmp == slots[cur_slot])
+                if (tmp == slots[cur_slot])
                 {
                     printf("delete header in cur_slot\n");
                     slots[cur_slot] = slots[cur_slot]->next;
                     delete tmp;
-                    if(slots[cur_slot])
+                    if (slots[cur_slot])
                     {
                         slots[cur_slot]->prev = NULL;
                     }
@@ -152,21 +168,18 @@ public:
                 else
                 {
                     tmp->prev->next = tmp->next;
-                    if(tmp->next)
+                    if (tmp->next)
                     {
                         tmp->next->prev = tmp->prev;
                     }
-                    tw_timer* tmp2 = tmp->next;
+                    tw_timer *tmp2 = tmp->next;
                     delete tmp;
                     tmp = tmp2;
-
                 }
-
             }
         }
-        cur_slot = ++cur_slot%N;
+        cur_slot = ++cur_slot % N;
     }
-
 
 private:
     static const int N = 60;
@@ -175,10 +188,219 @@ private:
     int cur_slot;
 };
 
+static time_wheel timeWheel;
+static int pipefd[2];
+static int epollfd;
 
-int main()
+
+int setnoblocking(int fd)
 {
-    
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+    return old_option;
+}
+
+void addfd(int epollfd, int fd)
+{
+    epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN | EPOLLET;
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    setnoblocking(fd);
+}
+
+void sig_handler(int sig)
+{
+    int save_errno = errno;
+    int msg = sig;
+    send(pipefd[1], (char *)&msg, 1, 0);
+    errno = save_errno;
+}
+
+void addsig(int sig)
+{
+    struct sigaction sa;
+    memset(&sa, '\0', sizeof(sa));
+    sa.sa_handler = sig_handler;
+    sa.sa_flags |= SA_RESTART;
+    sigfillset(&sa.sa_mask);
+    assert(sigaction(sig, &sa, NULL) != -1);
+}
+
+void cb_func(client_data *user_data)
+{
+    assert(user_data);
+    epoll_ctl(epollfd, EPOLL_CTL_DEL, user_data->sockfd, 0);
+    close(user_data->sockfd);
+    printf("close fd %d\n", user_data->sockfd);
+}
+
+void timer_handler()
+{
+    timeWheel.tick();
+    alarm(TIMESLOT);
+}
+
+
+int main(int argc, char *argv[])
+{
+    if (argc <= 2)
+    {
+        printf("usage:%s ip_address port_number backlog\n", basename(argv[0]));
+        return 1;
+    }
+
+    const char *ip = argv[1];
+    int port = atoi(argv[2]);
+
+    int ret = 0;
+
+    struct sockaddr_in address;
+    bzero(&address, sizeof(address));
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &address.sin_addr);
+    address.sin_port = htons(port);
+
+    int listenfd = socket(PF_INET, SOCK_STREAM, 0);
+    assert(listenfd >= 0);
+
+    ret = bind(listenfd, (struct sockaddr *)&address, sizeof(address));
+    assert(ret != -1);
+
+    ret = listen(listenfd, 5);
+    assert(ret != -1);
+
+    epoll_event events[MAX_EVENT_NUMBER];
+    int epollfd = epoll_create(5);
+    assert(epollfd != -1);
+    addfd(epollfd, listenfd);
+
+    ret = socketpair(PF_UNIX, SOCK_STREAM, 0, pipefd);
+    assert(ret != -1);
+    setnoblocking(pipefd[1]);
+
+    addfd(epollfd, pipefd[0]);
+
+    addsig(SIGALRM);
+    addsig(SIGTERM);
+
+    bool stop_server = false;
+
+    client_data *users = new client_data[FD_LIMIT];
+    bool timeout = false;
+    alarm(TIMESLOT);
+
+    while (!stop_server)
+    {
+        int number = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
+        if ((number < 0) && (errno != EINTR))
+        {
+            printf("epoll failure\n");
+            break;
+        }
+
+        for (int i = 0; i < number; i++)
+        {
+            int sockfd = events[i].data.fd;
+            if (sockfd == listenfd)
+            {
+                struct sockaddr_in client_address;
+                socklen_t client_addLength = sizeof(client_address);
+                int connfd = accept(listenfd, (struct sockaddr *)&client_address, &client_addLength);
+                addfd(epollfd, connfd);
+
+                tw_timer *pTimer = timeWheel.add_timer(3*TIMESLOT);
+                pTimer->cb_func = cb_func;
+                pTimer->user_data = &users[sockfd];
+                users[sockfd].timer = pTimer;
+            }
+            else if(sockfd == pipefd[0] && (events[i].events & EPOLLIN))
+            {
+                int sig;
+                char signals[1024];
+                ret = recv(pipefd[0],signals,sizeof(signals),0);
+                if(ret == -1 || ret == 0)
+                {
+                    continue;
+                }
+                else 
+                {
+                    for(int i = 0; i< ret; ++i)
+                    {
+                        switch(signals[i])
+                        {
+                            case SIGALRM:
+                            {
+                                timeout = true;
+                                break;
+                            }
+                            case SIGTERM:
+                            {
+                                stop_server = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            else if(events[i].events & EPOLLIN)
+            {
+                memset(users[sockfd].buf,'\0',BUFFER_SIZE);
+                ret = recv(sockfd,users[sockfd].buf,BUFFER_SIZE-1,0);
+                printf("get %d bytes of client data %s from %d\n",ret,users[sockfd].buf,sockfd);
+
+                 tw_timer *timer = users[sockfd].timer;
+
+                if(ret < 0)
+                {
+                    if(errno != EAGAIN)
+                    {
+                        cb_func(&users[sockfd]);
+                        if(timer)
+                        {
+                            timeWheel.del_timer(timer);
+                        }
+
+                    }
+                }
+                else if (ret == 0)
+                {
+                    cb_func(&users[sockfd]);
+                    if (timer)
+                    {
+                        timeWheel.del_timer(timer);
+                    }
+                }
+                else
+                {
+                    if (timer)
+                    {
+                        tw_timer *pTimer = timeWheel.add_timer(3 * TIMESLOT);
+                        pTimer->cb_func = cb_func;
+                        pTimer->user_data = &users[sockfd];
+                        users[sockfd].timer = pTimer;
+
+                        timeWheel.del_timer(timer);
+
+
+                    }
+                }
+            }
+        }
+
+        if(timeout)
+        {
+            timer_handler();
+            timeout = false;
+        }
+    }
+
+    close(listenfd);
+    close(pipefd[1]);
+    close(pipefd[0]);
+    delete []users;
+    return 0;
 }
 
 #endif
